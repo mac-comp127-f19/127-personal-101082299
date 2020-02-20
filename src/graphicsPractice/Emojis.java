75a7f64d3ccfef093c905a53c6a8c863f44f4e22
101082299 <ajohal@macalester.edu>
2020-02-19 18:14:57 -0600

    Emoji Vacation lab is ready to grade

---
+++ src/graphicsPractice/Emojis.java
@@ -0,0 +1,109 @@
+package graphicsPractice;
+
+import comp127graphics.*;
+
+import java.awt.Color;
+
+@SuppressWarnings("WeakerAccess")
+public class Emojis {
+    private static final Color
+        HEAD_COLOR = new Color(0xFFDE30),
+        HEAD_OUTLINE_COLOR = new Color(0xAC9620),
+        MOUTH_COLOR = new Color(0xE45B5B),
+        BLACK_COLOR = new Color(0);
+
+
+    public static void main(String[] args) {
+        CanvasWindow canvas = new CanvasWindow("Emoji", 800, 600);
+
+        GraphicsGroup littleSmiley = createSmileyFace(100);
+        littleSmiley.setPosition(50, 400);
+        canvas.add(littleSmiley);
+
+        GraphicsGroup mediumSmiley = createSmileyFace(200);
+        mediumSmiley.setPosition(150, 300);
+        canvas.add(mediumSmiley);
+
+//        GraphicsGroup bigSmiley = createSmileyFace(300);
+//        bigSmiley.setPosition(350, 200);
+//        canvas.add(bigSmiley);
+        GraphicsGroup reallyBro = createReallyFace(300);
+        reallyBro.setPosition(350, 200);
+        canvas.add(reallyBro);
+
+
+    }
+
+    /**
+     * Creates a smiley face emoji.
+     *
+     * @param size The overall width and height of the emoji.
+     * @return A graphic that you can add to a window, or place inside some other graphics group.
+     */
+    public static GraphicsGroup createSmileyFace(double size) {
+        GraphicsGroup group = new GraphicsGroup();
+
+        group.add(createHead(size, size));
+
+        // TODO: create eyes
+        Ellipse leftEye = new Ellipse(size *.25,size*.25, size*.1, size*.1);
+        leftEye.setFillColor(BLACK_COLOR);
+        group.add(leftEye);
+        Ellipse rightEye = new Ellipse(size *.75,size*.25, size*.1, size*.1);
+        rightEye.setFillColor(BLACK_COLOR);
+        group.add(rightEye);
+
+        Arc mouth = createSmile(size * 0.6, size * 0.5);
+        mouth.setCenter(size * 0.5, size * 0.75);
+        group.add(mouth);
+
+        return group;
+    }
+    public static GraphicsGroup createReallyFace(double size){
+        GraphicsGroup group = new GraphicsGroup();
+        group.add(createHead(size,size));
+
+        Rectangle leftEye = new Rectangle(size*.25, size*.25, size*.25, size*.01);
+        leftEye.setFillColor(BLACK_COLOR);
+        leftEye.setCenter(size*.3333,size*.3333);
+        group.add(leftEye);
+        Rectangle rightEye = new Rectangle(size*.7, size*.25, size*.25, size*.01);
+        rightEye.setFillColor(BLACK_COLOR);
+        rightEye.setCenter(size*.666666,size*.333333);
+        group.add(rightEye);
+
+        Rectangle mouth = new Rectangle(size*.7,size*.75,size*.4,size*.01);
+        mouth.setFillColor(BLACK_COLOR);
+        mouth.setCenter(size*.5,size*.5);
+        group.add(mouth);
+
+        return group;
+    }
+
+    /**
+     * Creates an empty emoji head. The head fits inside the box from (0,0)
+     * to (width,height).
+     */
+    private static Ellipse createHead(double height, double width) {
+        Ellipse head = new Ellipse(0, 0, width, height);
+        head.setFillColor(HEAD_COLOR);
+        head.setStrokeColor(HEAD_OUTLINE_COLOR);
+        head.setStrokeWidth(2);
+        return head;
+    }
+
+    /**
+     * Creates a smile-shaped arc. The arc is measured relative to its “implied ellipse,” which is
+     * the shape that would be formed if the arc were extend all the way around. The size of the
+     * resulting arc will be smaller than the implied ellipse’s size.
+     *
+     * @param ellipseWidth  The width of the implied ellipse from which the smile’s arc is cut.
+     * @param ellipseHeight The width of the implied ellipse from which the smile’s arc is cut.
+     */
+    private static Arc createSmile(double ellipseWidth, double ellipseHeight) {
+        Arc mouth = new Arc(0, 0, ellipseWidth, ellipseHeight, 200, 140);
+        mouth.setStrokeColor(MOUTH_COLOR);
+        mouth.setStrokeWidth(4);
+        return mouth;
+    }
+}
